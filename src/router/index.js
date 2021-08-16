d0555eb362c6883ade909199a7aba3f715873de2
xanny-bill <thapanon.sod@gmail.com>
2021-08-16 15:57:37 +0700

    test sum resolution

--- src/router/index.js
+++ src/router/index.js
@@ -18,14 +18,6 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
-  {
-    path: '/meeting',
-    name: 'Meeting',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Meeting.vue'),
-  },
 ];
 
 const router = new VueRouter({
