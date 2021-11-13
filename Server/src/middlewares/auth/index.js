80ce3f024fae92555697a0d28fc16644d36c9f59
xanny-bill <thapanon.sod@gmail.com>
2021-11-13 23:08:22 +0700

    set up esline for backend

--- Server/src/middlewares/auth/index.js
+++ Server/src/middlewares/auth/index.js
@@ -1,10 +1,7 @@
-const logger = (_req,_res,next )=>{
-  var ipaddress = (_req.headers['x-forwarded-for'] || 
-_req.connection.remoteAddress || 
-_req.socket.remoteAddress || 
-_req.connection.socket.remoteAddress).split(",")[0];
-console.log(_req.originalUrl)
+const logger = (_req, _res, next) => {
+  const ipaddress = (_req.headers['x-forwarded-for'] || _req.connection.remoteAddress || _req.socket.remoteAddress || _req.connection.socket.remoteAddress).split(',')[0];
+  console.log(`${_req.originalUrl} ${ipaddress}`);
   next();
-}
+};
 
-module.exports = logger
+module.exports = { logger };
