5072783e5a6f58cb9bd1589eac367fadcf990687
xanny-bill <thapanon.sod@gmail.com>
2021-11-16 16:28:03 +0700

    delete duplicate api

--- Server/src/router/auth.js
+++ Server/src/router/auth.js
@@ -7,7 +7,7 @@ router.post('/login', (req, res, next) => {
     if (err) return next(err);
 
     if (user) {
-      const token = jwt.sign(user, 'your_jwt_secret');
+      const token = jwt.sign(user, process.env.JWT_SECRET);
       return res.json({ user, token });
     }
     return res.status(422).json(info);
@@ -19,7 +19,7 @@ router.get('/google/callback',
   (req, res) => {
     const { user } = req;
     if (user) {
-      const token = jwt.sign(user, 'your_jwt_secret');
+      const token = jwt.sign(user, process.env.JWT_SECRET);
       return res.json({ user, token });
     }
     return res.status(422).json(user);
