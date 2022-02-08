3fa9d1e34e61cddc7f66eade17959612d5d45c85
RTae <natthanan.b@dia.co.th>
2022-02-08 21:32:01 +0700

    add : change file name

--- Client/src/router/index.js
+++ Client/src/router/index.js
@@ -22,7 +22,7 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: () => import('@/views/LoginT.vue'),
+    component: () => import('@/views/Login.vue'),
   },
   {
     path: '/signup',
