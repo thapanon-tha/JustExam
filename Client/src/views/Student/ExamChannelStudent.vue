a829087a65a1fd99892b8caeb53129cbf31661bb
xanny-bill <thapanon.sod@gmail.com>
2022-05-10 02:54:00 +0700

    formmat

--- Client/src/views/Student/ExamChannelStudent.vue
+++ Client/src/views/Student/ExamChannelStudent.vue
@@ -84,15 +84,13 @@
                 class="mt-1 box-border h-40 w-72 rounded-xl text-white bg-navyColor border-r-8 border-mainColor"
               >
                 <div class="flex flex-col m-2">
-                  <div
-                    class="h-28 w-42 p-2 text-orange-500"
-                    @click="onClick"
-                  >
+                  <div class="h-28 w-42 p-2 text-orange-500" @click="onClick">
                     <h1 class="text-sm font-semilight ml-2 truncate">
                       {{ channelsresult.description }}
                     </h1>
                     <h2 class="text-xs font-semilight ml-2">
-                      Exam date: {{ channelsresult.schedule | moment('D / MM / YYYY') }}
+                      Exam date:
+                      {{ channelsresult.schedule | moment('D / MM / YYYY') }}
                     </h2>
                     <h2 class="text-xs font-semilight ml-2">
                       Start at: {{ channelsresult.startAt | moment('H:mm:ss') }}
